a95e8613f8f423a2866e5f7ae34a3f56f6a83d1f
githubuchunemail <githubuchunemail@gmail.com>
2024-01-08 15:28:47 +0500

    create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Button } from 'react-bootstrap';
 import './App.css';
+import NavScrollExample from './Components/Header/Header';
 
 function App() {
   return (
     <>
-      
+      <NavScrollExample />
     </>
   );
 }
